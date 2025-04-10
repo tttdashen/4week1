6bfea471740590d5ae16e756303b47f5271ea831
tttdashen <2429043178@qq.com>
2025-04-10 10:07:53 +0800

    s1

---
+++ 4-week1.1/1.py
@@ -0,0 +1,19 @@
+a=-200
+print(abs(a))#abs是python函数，可以求绝对值
+a = [1,2,-4,5,8]
+print(max(a))
+a = '123'
+b = '12.3'
+print(int(a))
+print(float(b))#注意浮点数和整数转换不一样的，一个是float一个是int
+c= 123
+print(str(c))
+print(type(str(c)))#这里显示 <class 'str'>，证明 s 是字符串类型。
+d=1
+e=0
+f=[]
+print(bool(d))
+print(bool(e))
+print(bool(f))
+g=255
+print(hex(g))
