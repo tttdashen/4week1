8bb72fcff8d5064d6bf4eb2684230b940bf071d6
tttdashen <2429043178@qq.com>
2025-04-10 10:37:52 +0800

    参数检查

--- 4-week1.1/kong.py
+++ 4-week1.1/kong.py
@@ -33,3 +33,41 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+#参数检查
+from my_abs import my_abs
+#调用函数时，如果参数个数不对，Python解释器会自动检查出来，并抛出TypeError：
+#print(my_abs(1,2))
+
+#但是如果参数类型不对，Python解释器就无法帮我们检查。试试my_abs和内置函数abs的差别：
+
+# my_abs('A')
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#   File "<stdin>", line 2, in my_abs
+# TypeError: unorderable types: str() >= int()
+# >>> abs('A')
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: bad operand type for abs(): 'str'
+# 当传入了不恰当的参数时，内置函数abs会检查出参数错误，
+# 而我们定义的my_abs没有参数检查，会导致if语句出错，出错信息和abs不一样。所以，这个函数定义不够完善。
+#让我们修改一下my_abs的定义，对参数类型做检查，只允许整数和浮点数类型的参数。数据类型检查可以用内置函数isinstance()实现：
+
+# def my_abs(x):
+#     if not isinstance(x, (int, float)):
+#         raise TypeError('bad operand type')
+#     if x >= 0:
+#         return x
+#     else:
+#         return -x
+# if not isinstance(x, (int, float)):
+#     raise TypeError('bad operand type')
+# isinstance(x, (int, float)) 判断 x 是否属于 int 或 float 类型。
+
+# 如果 x 不是这两种类型之一，not 会使得条件成立，从而执行 raise TypeError('bad operand type') 语句。
+
+# raise 用来主动抛出异常，告诉调用者传入了不符合要求的参数类型。这种做法有助于及早发现错误，防止错误数据继续传播。
+
