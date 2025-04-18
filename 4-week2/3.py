dcf9efc4df23edfa7150b62d4ddf984d1429ea18
tttdashen <2429043178@qq.com>
2025-04-18 17:02:43 +0800

    test filter()-回数

--- 4-week2/3.py
+++ 4-week2/3.py
@@ -32,3 +32,25 @@ for n in primes():
         print(n)
     else:
         break
+
+#判断回数
+#str(n) == str(n)[::-1],  ::step是从头到尾遍历整个数列，setp是步长，-1即为从尾到头
+
+def is_palindrome(n):
+    # 判断 n 是否是回文数
+    return str(n) == str(n)[::-1]
+
+# 测试:
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+
+# 测试验证:
+if list(filter(is_palindrome, range(1, 200))) == [
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    11, 22, 33, 44, 55, 66, 77, 88, 99,
+    101, 111, 121, 131, 141, 151, 161, 171, 181, 191
+]:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
