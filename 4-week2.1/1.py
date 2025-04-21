23bb30ddd15b8f5b7c2bfe2ebf48a2d5f5cef8b8
tttdashen <2429043178@qq.com>
2025-04-21 16:52:16 +0800

    nonlocal

--- 4-week2.1/1.py
+++ 4-week2.1/1.py
@@ -64,3 +64,43 @@ print(g1(),g2(),g3())
 
 ✅ 第二种是：用函数把当前 i 的值传进来“锁定”到新变量 j 上，每个函数都保存了独立的 j，所以能得到正确的结果。
 '''
+
+#使用闭包，就是内层函数引用了外层函数的局部变量。如果只是读外层变量的值，我们会发现返回的闭包函数调用一切正常：
+def inx():
+    x=0
+    def inx_1():
+        return x+1
+    return inx_1
+f=inx()
+print(f())
+print(f())
+print(f())
+
+'''
+def inc():
+    x = 0
+    def fn():
+        # nonlocal x
+        x = x + 1
+        return x
+    return fn
+
+f = inc()
+print(f()) # 1
+print(f()) # 2
+#但是，如果对外层变量赋值，由于Python解释器会把x当作函数fn()的局部变量，它会报错
+'''
+#原因是x作为局部变量并没有初始化，直接计算x+1是不行的。但我们其实是想引用inc()函数内部的x，所以需要在fn()函数内部加一个nonlocal x的声明。
+#加上这个声明后，解释器把fn()的x看作外层函数的局部变量，它已经被初始化了，可以正确计算x+1。
+def inc():
+    x = 0
+    def fn():
+        nonlocal x
+        x = x + 1
+        return x
+    return fn
+
+f = inc()
+print(f()) # 1
+print(f()) # 2
+
